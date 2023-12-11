5fb987ca04ec98043b30ccbd14a9d4f8100e7391
sobercha <153471675+sobercha@users.noreply.github.com>
2023-12-11 01:11:46 -0500

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -103,6 +103,8 @@ X_train, X_test, y_train, y_test = train_test_split(X,
 lr = LogisticRegression(class_weight='balanced')
 lr.fit(X_train, y_train)
 
+y_pred = lr.predict(X_test)
+
 confusion_matrix(y_test,y_pred)
 
 newdata = pd.DataFrame({

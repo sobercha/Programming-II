eec062c3fe2b45747b5844d5a170470f6646545b
sobercha <153471675+sobercha@users.noreply.github.com>
2023-12-10 23:11:32 -0500

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -8,3 +8,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn import metrics
+
+s = pd.read_csv('social_media_usage.csv')
+

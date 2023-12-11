c927ee1507f9eaf5ed9b296e0e3800dec9d961bb
sobercha <153471675+sobercha@users.noreply.github.com>
2023-12-11 00:25:08 -0500

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 st.header('Welcome to my LinkedIn User Prediction App!')
 st.subheader('Please answer the questions below to generate your prediction:')
 
-income = st.radio('#1 What is your income level?:', ['1 - Less than $10,000','2 - 10 to under $20,000','3 - 20 to under $30,000','4 - 30 to under $40,000','5 - 40 to under $50,000','6 - 50 to under $75,000','7 - 75 to under $100,000','8 - 100 to under $150,000, OR','9 - $150,000+'])
+income = st.radio('#1 What is your income level?:', ['1 - Less than $10,000','2 - $10,000 to under $20,000','3 - $20,000 to under $30,000','4 - $30,000 to under $40,000','5 - $40,000 to under $50,000','6 - $50,000 to under $75,000','7 - $75,000 to under $100,000','8 - $100,000 to under $150,000','9 - $150,000+'])
 educ2 = st.radio('#2 What is your education level?:', ['1 - Less than high school (Grades 1-8 or no formal schooling)','2 - High school incomplete (Grades 9-11 or Grade 12 with NO diploma)','3 - High school graduate (Grade 12 with diploma or GED certificate)','4 - Some college, no degree (includes some community college)','5 - Two-year associate degree from a college or university','6 - Four-year college or university degree/Bachelor’s degree (e.g., BS, BA, AB)','7 - Some postgraduate or professional schooling, no postgraduate degree (e.g. some graduate school)','8 - Postgraduate or professional degree, including master’s, doctorate, medical or law degree (e.g., MA, MS, PhD, MD, JD)'])
 par = st.radio('#3 Are you a parent?:', ['1 - Yes','2 - No'])
 marital = st.radio('#4 Are you married?:', ['1 - Yes','2 - No'])
@@ -61,7 +61,6 @@ lr.fit(X_train, y_train)
 # Make predictions
 y_pred = lr.predict(X_test)
 
-# New data for features: income, education, parent, married, female, age
 person = ['income', 'educ2', 'par', 'marital', 'gender', 'age']
 
 # Predict class, given input features
